5c427bcf2659585a5d2e48ebe1aad1bb303c5c32
Mike Haggerty <mike.haggerty@airspacelink.com>
2021-12-03 15:36:27 -0500

    Updates

---
+++ precache-manifest.22df0f01f881cbf2b92a740e91bb41fa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2babb38c8f6221b811c95865c921f6ea",
+    "url": "/auth0-keycloak-test/index.html"
+  },
+  {
+    "revision": "fe7bed91395ad8b5a48b",
+    "url": "/auth0-keycloak-test/static/css/2.11b452ed.chunk.css"
+  },
+  {
+    "revision": "13c6394affc516cef9c8",
+    "url": "/auth0-keycloak-test/static/css/main.1aa3f63c.chunk.css"
+  },
+  {
+    "revision": "fe7bed91395ad8b5a48b",
+    "url": "/auth0-keycloak-test/static/js/2.0aa94306.chunk.js"
+  },
+  {
+    "revision": "713acd2dac6c7d6e42a0e19bc8c5c98d",
+    "url": "/auth0-keycloak-test/static/js/2.0aa94306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13c6394affc516cef9c8",
+    "url": "/auth0-keycloak-test/static/js/main.aab8f070.chunk.js"
+  },
+  {
+    "revision": "9d9f29a311c8b583c93c",
+    "url": "/auth0-keycloak-test/static/js/runtime-main.5d73922d.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/auth0-keycloak-test/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/auth0-keycloak-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
