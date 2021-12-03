ac3a486b38ef787526934ee7faf4b081c828956a
Mike Haggerty <mike.haggerty@airspacelink.com>
2021-12-03 15:41:02 -0500

    href

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const providerConfig = {
   domain: config.domain,
   clientId: config.clientId,
   ...(config.audience ? { audience: config.audience } : null),
-  redirectUri: window.location.origin,
+  redirectUri: window.location.href,
   onRedirectCallback,
 };
 
